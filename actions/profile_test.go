f002cae79d63c6f8526dc494817c7c7f4fdcab2f
cmdcarini <24967694+cmdcarini@users.noreply.github.com>
2018-10-29 11:46:11 -0500

    Fixed test and implemented profile route partially

--- actions/profile_test.go
+++ actions/profile_test.go
@@ -3,5 +3,5 @@ package actions
 func (as *ActionSuite) Test_ProfileHandler() {
 	res := as.JSON("/profile").Get()
 	as.Equal(200, res.Code)
-	as.Contains(res.Body.String(), "This is the signup page!")
+	as.Contains(res.Body.String(), "This is the profile page!")
 }
