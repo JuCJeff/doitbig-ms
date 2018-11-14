6b3c0a159743106cb840e4acf346e7d16d1518a2
cmdcarini <24967694+cmdcarini@users.noreply.github.com>
2018-11-14 12:07:59 -0600

    Updated some comments

--- actions/enroll.go
+++ actions/enroll.go
@@ -2,15 +2,15 @@ package actions
 
 import "github.com/gobuffalo/buffalo"
 
-// LoginHandler is a handler to serve up
-// a signup page.
+// EnrollGetHandler is a handler to manage course enrollment
 func EnrollGetHandler(c buffalo.Context) error {
-	return c.Render(200, r.JSON(map[string]string{"message": "This is the enroll page!" + 
-			"- you typed " + c.Param("uid") + " for your uid"}))
+	return c.Render(200, r.JSON(map[string]string{"message": "This is the enroll page!" +
+		"- you typed " + c.Param("uid") + " for your uid"}))
 }
 
+// EnrollPostHandler is a handler to manage course enrollment
 func EnrollPostHandler(c buffalo.Context) error {
-	return c.Render(200, r.JSON(map[string]string{"message": "This is the enroll page!" + 
-			"- you typed " + c.Param("uid") + " for your uid " + 
-			c.Param("cid") + " for your cid" }))
+	return c.Render(200, r.JSON(map[string]string{"message": "This is the enroll page!" +
+		"- you typed " + c.Param("uid") + " for your uid " +
+		c.Param("cid") + " for your cid"}))
 }
