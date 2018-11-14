0251f1a8b0bc6aeb0bc793905940fc44fec237c9
hyang344 <hyang344@wisc.edu>
2018-11-13 18:20:30 -0600

    Create EnrollGetHandler & Getting info from url

--- actions/app.go
+++ actions/app.go
@@ -62,6 +62,8 @@ func App() *buffalo.App {
 		app.GET("/profile", ProfileHandler)
 		app.GET("/signout", SignoutHandler)
 		app.GET("/login", LoginHandler)
+		app.GET("/enroll", EnrollGetHandler)
+		//app.POST("/enroll?uid={uid:[0-9]+}&cid={cid:[0-9]+}", EnrollPostHandler)
 	}
 
 	return app
