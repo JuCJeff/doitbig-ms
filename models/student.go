878ad17a0c6ed569d21d883609e05979f812a903
cmdcarini <24967694+cmdcarini@users.noreply.github.com>
2018-11-16 16:49:05 -0600

    Refactored models, and profile route

--- models/student.go
+++ models/student.go
@@ -1,11 +1,9 @@
 package models
 
-// Student model is a struct that contains the id, firstName, lastNAme, email, password, and image of student struct.
+// Student model is a struct that contains the id, coursetaken, upcomingcourse, and tracknum of user struct.
 type Student struct {
-	ID        int    `db:"id"`
-	FirstName string `db:"firstname"`
-	LastName  string `db:"lastname"`
-	Email     string `db:"email"`
-	Password  string `db:"password"`
-	Image     []byte `db:"image"`
+	ID             int    `db:"id"`
+	CourseTaken    string `db:"coursetaken"`
+	UpcomingCourse string `db:"upcomingcourse"`
+	TrackNum       int    `db:"tracknumber"`
 }
