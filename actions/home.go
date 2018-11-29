1b84d53bdf1a81d49794d87eb1fec596baa729c9
Yu Hao <hao43@wisc.edu>
2018-11-29 15:49:46 -0600

    tested redirecting

--- actions/home.go
+++ actions/home.go
@@ -8,10 +8,11 @@ func HomeHandler(c buffalo.Context) error {
 	value, err := c.Cookies().Get("user_id")
 	if err != nil {
 		//TODO: render login page if cookie not found
-		return c.Render(200, r.String("Here will be replaced by login page"))
+		c.Redirect(307, "http://localhost:3000/signin")
 	}
 	//if cookie found, then further proceed with the user_id
 	//this line if temporarily for Postman test with cookie return results
 	//TODO: extract required info from DB and render homepage
 	return c.Render(200, r.String(value))
+	//return c.Redirect(307, "http://localhost:3000")
 }
