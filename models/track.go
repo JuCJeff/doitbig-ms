b4ffdbbf6f0b04bc773b3f252350077160661d9d
Yu Hao <hao43@wisc.edu>
2018-11-30 14:56:31 -0600

    modified course.go and track.go in models so now they contain the Image variable that corresponds to the file path of images stored in db

--- models/track.go
+++ models/track.go
@@ -2,6 +2,7 @@ package models
 
 // Track model is a struct that contains the id and the track name.
 type Track struct {
-	ID   int    `db:"id"`
-	Name string `db:"name"`
+	ID    int    `db:"id"`
+	Name  string `db:"name"`
+	Image string `db:"image"`
 }
