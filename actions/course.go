7be34da02cde705326e464c966148e9caa91bd11
Yu Hao <hao43@wisc.edu>
2018-11-30 14:25:11 -0600

    if frontend called course route with no param, will return all courses in db, if frontend called course route with a specific param, will return only one course

--- actions/course.go
+++ actions/course.go
@@ -9,8 +9,16 @@ import (
 // CourseHandler is a handler to serve up
 // a course page.
 func CourseHandler(c buffalo.Context) error {
-	course := models.Course{}
-	db := c.Value("tx").(*pop.Connection)
-	db.Find(&course, c.Param("cid"))
-	return c.Render(200, r.JSON(course))
+	if c.Param("cid") == "" {
+		courses := []models.Course{}
+		db := c.Value("tx").(*pop.Connection)
+		db.All(&courses)
+		return c.Render(200, r.JSON(courses))
+
+	} else {
+		course := models.Course{}
+		db := c.Value("tx").(*pop.Connection)
+		db.Find(&course, c.Param("cid"))
+		return c.Render(200, r.JSON(course))
+	}
 }
