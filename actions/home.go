d90261ec4e6cf658aa82a996a6d770ed59877da1
Jeff Ma <zma67@wisc.edu>
2018-11-30 14:29:16 -0600

    Change course information to more specific contents

--- actions/home.go
+++ actions/home.go
@@ -5,7 +5,7 @@ import "github.com/gobuffalo/buffalo"
 // HomeHandler is a default handler to serve up
 // a home page.
 func HomeHandler(c buffalo.Context) error {
-	trackOne := "STS Track One"
+	trackOne := "Web Developer Track"
 	trackTwo := "STS Track Two"
 	trackThree := "STS Track Three"
 	trackOneDes := "STS Track One description goes here."
@@ -14,7 +14,7 @@ func HomeHandler(c buffalo.Context) error {
 	courseOne := "STS Course One"
 	courseTwo := "STS Course Two"
 	courseThree := "STS Course Three"
-	courseOneDes:= "STS Course One Description goes here."
+	courseOneDes := "STS Course One Description goes here."
 	courseTwoDes := "STS Course Two Description goes here."
 	courseThreeDes := "STS Course Three Description goes here."
 	return c.Render(200, r.JSON(map[string]string{
@@ -23,11 +23,11 @@ func HomeHandler(c buffalo.Context) error {
 		"track_2":      trackTwo,
 		"track_2_des":  trackTwoDes,
 		"track_3":      trackThree,
-		"track_3_des": 	trackThreeDes,
-		"course_1": 	courseOne,
+		"track_3_des":  trackThreeDes,
+		"course_1":     courseOne,
 		"course_1_des": courseOneDes,
-		"course_2": 	courseTwo,
+		"course_2":     courseTwo,
 		"course_2_des": courseTwoDes,
-		"course_3": 	courseThree,
+		"course_3":     courseThree,
 		"course_3_des": courseThreeDes}))
 }
